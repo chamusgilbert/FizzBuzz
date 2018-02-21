0f007b708f336692564ab209a057c3073b9c14e2
famouschamus <chamusg@gmail.com>
2018-02-21 14:00:41 -0500

    Initial Commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace FizzBuzz
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                //Method1(i);
+                //Method2(i);
+                Method3(i);
+            }
+        }
+        public static void Method1(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        public static void Method2(int i)
+        {
+            string str = "";
+            if (i % 3 == 0)
+            {
+                str += "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                str += "Buzz";
+            }
+            if (str.Length == 0)
+            {
+                str = i.ToString();
+            }
+            Console.WriteLine(str);
+
+        }
+        public static void Method3(int i)
+        {//This Method is created to run backwards, ie. "BuzzFizz".
+            string str = "";
+            if (i % 3 == 0)
+            {
+                str += "Buzz";
+            }
+            if (i % 5 == 0)
+            {
+                str += "Fizz";
+            }
+            if (str.Length == 0)
+            {
+                str = i.ToString();
+            }
+            Console.WriteLine(str);
+
+        }
+    }
+}
---
+++ FizzBuzz/TDD.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    class TDD
+    {
+        [Test]
+        public static void TestDriven()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                //Program.Method1(i);
+                Program.Method2(i);
+                Program.Method3(i);
+            }
+        }
+    }
+}
